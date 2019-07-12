13924dd50c410166ae051496c697a1858aa7d6fb
David Brooks <dave@bcs.co.nz>
2019-07-12 20:17:09 +1200

    The metadata field is called `created`...

--- mapmaker.py
+++ mapmaker.py
@@ -208,7 +208,7 @@ if __name__ == '__main__':
         tile_db.add_metadata(annotations=json.dumps(annotations))
 
         # Save the maps creation time
-        tile_db.add_metadata(creation=datetime.datetime.utcnow().isoformat())
+        tile_db.add_metadata(created=datetime.datetime.utcnow().isoformat())
 
         # Commit updates to the database
         tile_db.execute("COMMIT")
