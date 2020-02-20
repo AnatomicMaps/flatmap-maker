a0653054604021aef5bc6af72e26eac1cef39596
David Brooks <dave@bcs.co.nz>
2020-02-21 12:10:26 +1300

    Work on packaging `mapmaker` as a Python package.

---
+++ setup.py
@@ -0,0 +1,164 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+Modified by Madoshakalaka@Github (dependency links added)
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    # This is the name of your project. The first time you publish this
+    # package, this name will be registered for you. It will determine how
+    # users can install this project, e.g.:
+    #
+    # $ pip install sampleproject
+    #
+    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
+    #
+    # There are some restrictions on what makes a valid project name
+    # specification here:
+    # https://packaging.python.org/specifications/core-metadata/#name
+    name="mapmaker",  # Required
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    #
+    # For a discussion on single-sourcing the version across setup.py and the
+    # project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version="0.7.0",  # Required
+    # This is a one-line description or tagline of what your project does. This
+    # corresponds to the "Summary" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#summary
+    description="Convert Powerpoint slides to Mapbox tiles",  # Optional
+    # This should be a valid link to your project's main homepage.
+    #
+    # This field corresponds to the "Home-Page" metadata field:
+    # https://packaging.python.org/specifications/core-metadata/#home-page-optional
+    url="https://github.com/dbrnz/flatmap-mvt-maker",  # Optional
+    # This should be your name or the name of the organization which owns the
+    # project.
+    author="David Brooks",  # Optional
+    # This should be a valid email address corresponding to the author listed
+    # above.
+    author_email="d.brooks@auckland.ac.nz",  # Optional
+    # Classifiers help users find your project by categorizing it.
+    #
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        # Pick your license as you wish
+        "License :: OSI Approved :: Apache Software License",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # These classifiers are *not* checked by 'pip install'. See instead
+        # 'python_requires' below.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # This field adds keywords for your project which will appear on the
+    # project page. What does your project relate to?
+    #
+    # Note that this is a string of words separated by whitespace, not a list.
+    keywords="TODO...",  # Optional
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=["src", "src.drawml"],  # Required
+    # Specify which Python versions you support. In contrast to the
+    # 'Programming Language' classifiers above, 'pip install' will check this
+    # and refuse to install the project if the version does not match.
+    python_requires=">=3.5, <4",
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        "alembic==1.4.0",
+        "beziers==0.0.1",
+        "certifi==2019.11.28",
+        "chardet==3.0.4",
+        "click==7.0",
+        "commentjson==0.8.3",
+        "idna==2.9",
+        "isodate==0.6.0",
+        "lark-parser==0.7.8",
+        "lxml==4.5.0",
+        "mako==1.1.1",
+        "markupsafe==1.1.1",
+        "mbutil==0.3.0",
+        "mercantile==1.1.2",
+        "numpy==1.18.1",
+        "owlready2==0.23",
+        "pillow==7.0.0",
+        "pymupdf==1.16.10",
+        "pyparsing==2.4.6",
+        "pyproj==2.4.2.post1",
+        "python-dateutil==2.8.1",
+        "python-editor==1.0.4",
+        "python-pptx==0.6.18",
+        "pyyaml==5.3",
+        "rdflib==4.2.2",
+        "rdflib-sqlalchemy==0.4.0",
+        "requests==2.23.0",
+        "shapely==1.7.0",
+        "six==1.14.0",
+        "sqlalchemy==1.3.13",
+        "svgwrite==1.3.1",
+        "urllib3==1.25.8",
+        "xlsxwriter==1.2.7",
+    ],  # Optional
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={"dev": []},  # Optional
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    #
+    # Sometimes you’ll want to use packages that are properly arranged with
+    # setuptools, but are not published to PyPI. In those cases, you can specify
+    # a list of one or more dependency_links URLs where the package can
+    # be downloaded, along with some additional hints, and setuptools
+    # will find and install the package correctly.
+    # see https://python-packaging.readthedocs.io/en/latest/dependencies.html#packages-not-on-pypi
+    #
+    dependency_links=[],
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/dbrnz/flatmap-mvt-maker/issues",
+        "Source": "https://github.com/dbrnz/flatmap-mvt-maker/",
+    },
+)
