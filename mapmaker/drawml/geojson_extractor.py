5b7f86e4002b0acb8174bad0cfdbfab91bec8c9f
David Brooks <dave@bcs.co.nz>
2020-04-22 15:32:05 +1200

    Use first region that is inside a polygon to set the polygon's properties.

--- mapmaker/drawml/geojson_extractor.py
+++ mapmaker/drawml/geojson_extractor.py
@@ -214,6 +214,7 @@ class GeoJsonLayer(Layer):
                     for region in filter(lambda p: prepared_polygon.contains(p.geometry), regions):
                         region_id = region.id
                         region_properties.update(region.properties)
+                        break
                     if region_id is None:
                         region_id = self.__region_id
                         self.__region_id += 1
