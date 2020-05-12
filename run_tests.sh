e080884f1518e51b043dac823f30cec82b590706
David Brooks <dave@bcs.co.nz>
2020-05-13 09:16:14 +1200

    Add a `spinal_cord` test.

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-./run_test.sh heart      Rat_heart_annotated.pptx
-./run_test.sh intestine  rat_intestine.pptx
-./run_test.sh spine-bit  rat_spine_bit.pptx
-./run_test.sh spine-end1 rat_spine_end1.pptx
+./run_test.sh heart       Rat_heart_annotated.pptx
+./run_test.sh intestine   rat_intestine.pptx
+./run_test.sh spine-bit   rat_spine_bit.pptx
+./run_test.sh spine-end1  rat_spine_end1.pptx
+./run_test.sh spinal-cord rat_flatmap_spinal_cord_closed.pptx
 
