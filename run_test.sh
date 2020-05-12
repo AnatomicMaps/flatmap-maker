fa445559ef3f9d9032bc637e92bb2c00de50852a
David Brooks <dave@bcs.co.nz>
2020-05-13 09:15:53 +1200

    Keep PPTs used for testing separate from general development.

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pipenv run python mapmaker --id $1  \
-                           --slides ./map_sources/tests/$2 \
+                           --slides ./tests/sources/$2 \
                            --map-dir ./maps
