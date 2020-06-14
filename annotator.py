692ca89b2932f8cf17ab4c32b69c1d7ae6ec889f
David Brooks <dave@bcs.co.nz>
2020-06-14 22:41:11 +1200

    Add a `--names-only` option to annotator tool to only output shape names (i.e. with no property lookup).

--- annotator.py
+++ annotator.py
@@ -34,8 +34,9 @@ from mapmaker.properties import Properties
 #===============================================================================
 
 class Slide(object):
-    def __init__(self, slide, properties):
+    def __init__(self, slide, properties, options):
         self.__properties = properties
+        self.__names_only = options.names_only
         self.__slide_id = slide.slide_id
         if slide.has_notes_slide:
             notes_slide = slide.notes_slide
@@ -49,7 +50,6 @@ class Slide(object):
 
     def process_shape_list_(self, shapes, outermost):
         if outermost:
-            print('Processing shape list...')
             progress_bar = tqdm(total=len(shapes),
                 unit='shp', ncols=40,
                 bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}')
@@ -67,21 +67,23 @@ class Slide(object):
             progress_bar.close()
 
     def list(self):
-        print('SLIDE: {!s:8} {}'.format(self.__slide_id, self.__notes))
         for id, properties in self.__properties_by_id.items():
             if properties and properties.get('shape-name', ' ')[0] in ['#', '.']:
-                print('SHAPE: {:8} {}'.format(id, properties))
+                if self.__names_only:
+                    print('{:8} {}'.format(id, properties['shape-name']))
+                else:
+                    print('{:8} {}'.format(id, properties))
         print('')
 
 #===============================================================================
 
 class Presentation(object):
-    def __init__(self, powerpoint, properties):
+    def __init__(self, powerpoint, properties, options):
         self.__pptx = pptx.Presentation(powerpoint)
         self.__slides_by_id = OrderedDict()
         self.__seen = []
         for slide in self.__pptx.slides:
-            self.__slides_by_id[slide.slide_id] = Slide(slide, properties)
+            self.__slides_by_id[slide.slide_id] = Slide(slide, properties, options)
 
     def list(self):
         for slide in self.__slides_by_id.values():
@@ -103,6 +105,9 @@ if __name__ == '__main__':
     parser.add_argument('--properties',
                         help='JSON file specifying additional properties of shapes')
 
+    parser.add_argument('-n', '--names-only', action='store_true',
+                        help='Only list shape names')
+
     # Options `--replace`
     # specify slide
     #         shape by id/class
@@ -116,5 +121,5 @@ if __name__ == '__main__':
     args.label_database = 'labels.sqlite'
     external_properties = Properties(args)
 
-    presentation = Presentation(args.powerpoint, external_properties)
+    presentation = Presentation(args.powerpoint, external_properties, args)
     presentation.list()
