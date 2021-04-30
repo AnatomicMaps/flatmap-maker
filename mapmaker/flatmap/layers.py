4165a83b9b00440e931eec2541a05fc697351786
David Brooks <dave@bcs.co.nz>
2021-04-30 12:08:14 +1200

    A group of features will have a set of `bezier-paths` with each path being a number of `bezier-segments`.

--- mapmaker/flatmap/layers.py
+++ mapmaker/flatmap/layers.py
@@ -306,16 +306,16 @@ class MapLayer(FeatureLayer):
             for feature in layer_features:
                 grouped_polygon_features.append(feature)
 
-            bezier_segments = []
+            bezier_paths = []
             grouped_lines = []
             for feature in grouped_polygon_features:
                 if feature.get_property('tile-layer') != 'pathways':
                     if feature.geom_type == 'LineString':
                         grouped_lines.append(feature.geometry)
-                        bezier_segments.extend(feature.get_property('bezier-segments', []))
+                        bezier_paths.append(feature.get_property('bezier-segments', []))
                     elif feature.geom_type == 'MultiLineString':
                         grouped_lines.extend(list(feature.geometry))
-                        bezier_segments.extend(feature.get_property('bezier-segments', []))
+                        bezier_paths.extend(feature.get_property('bezier-paths', []))
             if len(grouped_lines):
                 feature_group = self.flatmap.new_feature(
                       shapely.geometry.MultiLineString(grouped_lines),
@@ -325,18 +325,18 @@ class MapLayer(FeatureLayer):
             for feature in grouped_polygon_features:
                 if feature.geom_type == 'Polygon':
                     grouped_polygons.append(feature.geometry)
-                    bezier_segments.extend(feature.get_property('bezier-segments', []))
+                    bezier_paths.append(feature.get_property('bezier-segments', []))
                 elif feature.geom_type == 'MultiPolygon':
                     grouped_polygons.extend(list(feature.geometry))
-                    bezier_segments.extend(feature.get_property('bezier-segments', []))
+                    bezier_paths.extend(feature.get_property('bezier-paths', []))
             if len(grouped_polygons):
                 feature_group = self.flatmap.new_feature(
-                        shapely.geometry.MultiPolygon(grouped_polygons),
+                        shapely.geometry.MultiPolygon(grouped_polygons).buffer(0),
                         grouped_properties, True)
                 layer_features.append(feature_group)
 
-            if feature_group is not None and len(bezier_segments):
-                feature_group.set_property('bezier-segments', bezier_segments)
+            if feature_group is not None and len(bezier_paths):
+                feature_group.set_property('bezier-paths', bezier_paths)
 
         # Feature specific properties have precedence over group's
 
