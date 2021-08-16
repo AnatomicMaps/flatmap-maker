779e0621cc05d002449639255a20b454cd1b6be3
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2021-08-17 10:27:43 +1200

    Add type for Py3 compatibility.

--- mapmaker/routing/utils/maths.py
+++ mapmaker/routing/utils/maths.py
@@ -1,27 +1,27 @@
 from math import sqrt
 
 
-def add(u, v):
+def add(u: list, v: list) -> list:
     return [u[i] + v[i] for i in range(len(u))]
 
 
-def magnitude(v):
+def magnitude(v: list):
     return sqrt(sum(v[i] * v[i] for i in range(len(v))))
 
 
-def mult(u, c):
+def mult(u: list, c):
     return [u[i] * c for i in range(len(u))]
 
 
-def normalize(v):
+def normalize(v: list):
     vmag = magnitude(v)
     return [v[i] / vmag for i in range(len(v))]
 
 
-def set_magnitude(v, mag):
+def set_magnitude(v: list, mag):
     scale = mag / sqrt(sum(c * c for c in v))
     return [c * scale for c in v]
 
 
-def sub(u, v):
+def sub(u: list, v: list):
     return [u[i] - v[i] for i in range(len(u))]
