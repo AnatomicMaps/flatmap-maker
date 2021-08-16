54481ae6b140835da62f32f48d8f422ddb68290f
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2021-08-17 10:28:37 +1200

    Changes to account for automated routing.

--- mapmaker/routing/network.py
+++ mapmaker/routing/network.py
@@ -1,4 +1,4 @@
-#===============================================================================
+# ===============================================================================
 #
 #  Flatmap viewer and annotation tools
 #
@@ -16,31 +16,33 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-#===============================================================================
+# ===============================================================================
 
-from .region import Roads
-
-#===============================================================================
-
+# ===============================================================================
 
 import beziers.path
 import shapely.geometry
 
-#===============================================================================
+# ===============================================================================
 
 from mapmaker.geometry import bezier_sample
 from mapmaker.settings import settings
 from mapmaker.utils import log
+from mapmaker.routing.routes import Sheath
+from mapmaker.routing.neurons import Connectivity
 
-#===============================================================================
+
+# ===============================================================================
 
 class RouteSegment(object):
-    def __init__(self, path_id, node_set, nodes_geometry, edge_geometry, path_type):
+    def __init__(self, path_id, node_set, nodes_geometry, edge_geometry, path_type, roads, nerve):
         self.__id = path_id
         self.__node_set = node_set
         self.__nodes_geometry = nodes_geometry
         self.__edge_geometry = edge_geometry
         self.__path_type = path_type
+        self.__sheaths = roads
+        self.__nerve = nerve
 
     @property
     def id(self):
@@ -54,11 +56,16 @@ class RouteSegment(object):
         path_layout = settings.get('pathLayout', 'automatic')
         if path_layout == 'linear':
             return shapely.geometry.MultiLineString(
-                [ shapely.geometry.LineString([ node.centroid for node in nodes ])
-                    for nodes in self.__nodes_geometry ])
+                [shapely.geometry.LineString([node.centroid for node in nodes])
+                 for nodes in self.__nodes_geometry])
         elif path_layout == 'automatic':
-            # Automatic routing magic goes in here...
-            pass
+            log("Automated pathway layout...")
+            sheath_scaffold, evaluate_settings = self.__sheaths.get_sheath(self.__nerve, self.__node_set)
+            sheath_scaffold.generate()
+            connectivity = Connectivity(self.__id, sheath_scaffold, evaluate_settings)
+            auto_beziers = connectivity.get_neuron_line_beziers()
+            path = beziers.path.BezierPath.fromSegments(auto_beziers)
+            return shapely.geometry.LineString(bezier_sample(path))
         # Fallback is centreline layout
         path = beziers.path.BezierPath.fromSegments(self.__edge_geometry)
         return shapely.geometry.LineString(bezier_sample(path))
@@ -69,13 +76,18 @@ class RouteSegment(object):
             'type': 'line-dash' if self.__path_type.endswith('-post') else 'line'
         }
 
-#===============================================================================
+# ===============================================================================
+
 
 class NetworkRouter(object):
     def __init__(self, networks, edges, nodes):
         self.__networks = networks
         self.__edges = edges
         self.__nodes = nodes
+        self.__sheath_scaffolds = []
+
+        self.__sheaths = Sheath(networks, edges, nodes)
+        self.__sheaths.build()
 
     def layout(self, model, path_connections):
         network = self.__networks.get(model, {})
@@ -91,23 +103,40 @@ class NetworkRouter(object):
             }
             '''
             nodes_list = [network.get(edge) for edge in pathway['paths']]
+            if len(nodes_list[0]) > 2:
+                nodes_list[0] = [nodes_list[0][0], nodes_list[0][1]]
             node_set = set(nodes_list[0])
             for nodes in nodes_list[1:]:
+                if len(nodes) > 2:
+                    nodes = nodes[nodes[0], nodes[1]]
                 node_set.update(nodes)
             if pathway['start'] != nodes_list[0][0]:
                 log.error("Start node doesn't match path start for '{}'".format(pathway['id']))
             if pathway['end'] != nodes_list[-1][-1]:
                 log.error("End node doesn't match path end for '{}'".format(pathway['id']))
-            route_segments[pathway['id']] = RouteSegment(pathway['id'], node_set,
-                                                         [[self.__nodes.get(node) for node in nodes]
-                                                            for nodes in nodes_list],
-                                                         [self.__edges.get(edge)
-                                                            for edge in pathway['paths']],
-                                                         pathway['type'])
+            path_layout = settings.get('pathLayout', 'automatic')
+            if path_layout == 'automatic':
+                route_segments[pathway['id']] = RouteSegment(pathway['id'], node_set,
+                                                             [[self.__nodes.get(node) for node in nodes]
+                                                              for nodes in nodes_list],
+                                                             [self.__edges.get(edge)
+                                                              for edge in pathway['paths']],
+                                                             pathway['type'], self.__sheaths, model)
 
-        return { connection['id']: [ route_segments.get(pathway)
-                                        for pathway in connection['pathways']]
-            for connection in path_connections['connections']}
+            else:
+                route_segments[pathway['id']] = RouteSegment(pathway['id'], node_set,
+                                                             [[self.__nodes.get(node) for node in nodes]
+                                                              for nodes in nodes_list],
+                                                             [self.__edges.get(edge)
+                                                              for edge in pathway['paths']],
+                                                             pathway['type'], self.__sheaths, model)
+
+
+
+
+        return {connection['id']: [route_segments.get(pathway)
+                                   for pathway in connection['pathways']]
+                for connection in path_connections['connections']}
         '''
         {
             "id": "connection_1",
@@ -115,4 +144,4 @@ class NetworkRouter(object):
         }
         '''
 
-#===============================================================================
+# ===============================================================================
