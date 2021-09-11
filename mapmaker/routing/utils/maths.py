433cbfb6706549a165e6bc2d4828487f47ba1088
David Brooks <dave@bcs.co.nz>
2021-09-12 09:45:49 +1200

    Tidy up code to be more Pythonic.

--- mapmaker/routing/utils/maths.py
+++ mapmaker/routing/utils/maths.py
@@ -1,27 +1,27 @@
+#===============================================================================
+
 from math import sqrt
 
+#===============================================================================
 
 def add(u: list, v: list) -> list:
-    return [u[i] + v[i] for i in range(len(u))]
+    return [x + v[n] for n, x in enumerate(u)]
 
+def magnitude(v: list) -> float:
+    return sqrt(sum(x * x for x in v))
 
-def magnitude(v: list):
-    return sqrt(sum(v[i] * v[i] for i in range(len(v))))
+def mult(u: list, c: float) -> list:
+    return [x * c for x in u]
 
-
-def mult(u: list, c):
-    return [u[i] * c for i in range(len(u))]
-
-
-def normalize(v: list):
+def normalize(v: list) -> list:
     vmag = magnitude(v)
-    return [v[i] / vmag for i in range(len(v))]
+    return [x / vmag for x in v]
 
+def set_magnitude(v: list, mag: float) -> list:
+    scale = mag / magnitude(v)
+    return [x * scale for x in v]
 
-def set_magnitude(v: list, mag):
-    scale = mag / sqrt(sum(c * c for c in v))
-    return [c * scale for c in v]
+def sub(u: list, v: list) -> list:
+    return [x - v[n] for n, x in enumerate(u)]
 
-
-def sub(u: list, v: list):
-    return [u[i] - v[i] for i in range(len(u))]
+#===============================================================================
