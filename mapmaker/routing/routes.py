9237f10ee0d35e8d7f420339ebcbfe1c448bb0c3
David Brooks <dave@bcs.co.nz>
2021-09-19 11:06:21 +1200

    Join centrelines by trimming the ends to be joined and constructing a Bezier curve between the trimmed ends.

--- mapmaker/routing/routes.py
+++ mapmaker/routing/routes.py
@@ -22,9 +22,13 @@ from collections import defaultdict
 
 #===============================================================================
 
+from beziers.cubicbezier import CubicBezier
 from beziers.path import BezierPath
+from beziers.point import Point as BezierPoint
+
 import networkx as nx
 import numpy as np
+import shapely.geometry
 
 #===============================================================================
 
@@ -60,6 +64,102 @@ Bezier_to_Hermite = np.array([[ 1,  0,  0,  0],
 
 #===============================================================================
 
+def point_from_bezier(index, bezier):
+    point = bezier.points[index]
+    return np.array((point.x, point.y))
+
+def split_bezier(bezier, divisions):
+#===================================
+    if divisions == 1:
+        return [bezier]
+    elif divisions % 2 == 1:
+        parts = bezier.splitAtTime(1.0/divisions)
+        return [parts[0]] + split_bezier(parts[1], divisions - 1)
+    else:
+        parts = bezier.splitAtTime(0.5)
+        return split_bezier(parts[0], divisions//2) + split_bezier(parts[1], divisions//2)
+
+def trim_bezier_to_region_edge(bezier, region, start=True):   ## region = Circle(node_centre, RADIUS)
+#=========================================================
+    mid_point = bezier.pointAtTime(0.5)
+    assert not region.contains(shapely.geometry.Point([mid_point.x, mid_point.y]))
+
+    end_point = bezier.pointAtTime(0.0 if start else 1.0)
+    if not region.contains(shapely.geometry.Point([end_point.x, end_point.y])):
+        pass
+        # Either curve passes through region (double intercept):
+            # bezier.distance_to(node_centre) <= RADIUS
+        # Or just isn't long enough:
+            # Extend bezier until end is in region by setting end point
+            # to region's centroid (node_centre)
+
+        # BezierPath.distanceToPath(other, samples=10)
+            # Finds the distance to the other curve at its closest point,
+            # along with the t values for the closest point at each segment
+            # and the relevant segments.
+            #
+            # Returns: distance, t1, t2, seg1, seg2.
+
+    EPSILON = 0.01
+    if start:
+        t_start = 0.0
+        t_end = 0.5
+        while (t_end - t_start) > EPSILON:
+            t_mid = (t_start + t_end)/2
+            mid_point = bezier.pointAtTime(t_mid)
+            if region.contains(shapely.geometry.Point([mid_point.x, mid_point.y])):
+                t_start = t_mid
+            else:
+                t_end = t_mid
+    else:
+        t_start = 0.5
+        t_end = 1.0
+        while (t_end - t_start) > EPSILON:
+            t_mid = (t_start + t_end)/2
+            mid_point = bezier.pointAtTime(t_mid)
+            if region.contains(shapely.geometry.Point([mid_point.x, mid_point.y])):
+                t_end = t_mid
+            else:
+                t_start = t_mid
+    parts = bezier.splitAtTime(t_mid)
+    return parts[1 if start else 0]
+
+def line_intersection(line_points_1, line_points_2):
+#===================================================
+    line_1 = shapely.geometry.LineString([[line_points_1[0].x, line_points_1[0].y],
+                                          [line_points_1[1].x, line_points_1[1].y]])
+    line_2 = shapely.geometry.LineString([[line_points_2[0].x, line_points_2[0].y],
+                                          [line_points_2[1].x, line_points_2[1].y]])
+    while not line_1.intersects(line_2):
+        line_1 = shapely.affinity.scale(line_1, 10, 10)
+        line_2 = shapely.affinity.scale(line_2, 10, 10)
+    intersection = line_1.intersection(line_2)
+    return intersection.coords[0]
+
+def join_beziers_in_region(last_bezier, join_region, next_bezier):
+#=================================================================
+    last_bezier = trim_bezier_to_region_edge(last_bezier.asSegments()[-1], join_region, start=False)
+    next_bezier = trim_bezier_to_region_edge(next_bezier.asSegments()[0], join_region, start=True)
+
+    # now want intersection of derivative lines
+    intersection = line_intersection(last_bezier.points[2:4], next_bezier.points[0:2])
+    intersection = BezierPoint(*intersection)
+    #join_bezier = CubicBezier(last_bezier.points[3], intersection, intersection, next_bezier.points[0])
+
+    ## What if intersection is outside of join_region? Or even not near the region's centre??
+    region_centre = BezierPoint(*join_region.centroid.coords[0])
+    join_bezier = CubicBezier(last_bezier.points[3], region_centre, region_centre, next_bezier.points[0])
+
+    ## Adjust derivatives at end/start of last/next bezier to have same magnitudes
+    ## as join_bezier's start/end derivative (slope will already match so simply set as
+    ## last_bezier.points[2] = (last_bezier.points[3] - (intersection - last_bezier.points[3]))
+    ## etc...
+
+    return (last_bezier, join_bezier, next_bezier)
+
+#===============================================================================
+#===============================================================================
+
 class ControlPoint(object):
     def __init__(self, position: np.array, derivative: np.array):
         self.__position = position
@@ -85,20 +185,14 @@ class ControlPoint(object):
 #===============================================================================
 
 class ControlPointList(list):
-
-    @staticmethod
-    def split_bezier(bezier, divisions):
-        if divisions == 1:
-            return [bezier]
-        elif divisions % 2 == 1:
-            parts = bezier.splitAtTime(1.0/divisions)
-            return [parts[0]] + ControlPointList.split_bezier(parts[1], divisions - 1)
-        else:
-            parts = bezier.splitAtTime(0.5)
-            return ControlPointList.split_bezier(parts[0], divisions//2) + ControlPointList.split_bezier(parts[1], divisions//2)
+    def __init__(self, beziers=None):
+        super().__init__()
+        if beziers is not None:
+            for bezier in beziers:
+                self.append(bezier)
 
     def append(self, bezier):
-        for bezier in self.split_bezier(bezier, NUMBER_OF_BEZIER_PARTS):
+        for bezier in split_bezier(bezier, NUMBER_OF_BEZIER_PARTS):
             hermite = Bezier_to_Hermite@[[p.x, p.y] for p in bezier.points]
             if len(self) == 0:
                 super().append(ControlPoint(hermite[0], hermite[1]))
@@ -111,16 +205,27 @@ class ControlPointList(list):
 class PathSegment(object):
     def __init__(self, start_region, connecting_path, end_region):
         self.__start_region = start_region
-        self.__start_point = np.array(start_region.centroid.coords[0])
         self.__end_region = end_region
-        self.__end_point = np.array(end_region.centroid.coords[0])
-        self.__control_points = ControlPointList()
-        if connecting_path is None:
-            pass
-        else:
+        if connecting_path is not None:
             bezier_segments = connecting_path.asSegments()
-            for bezier in bezier_segments:
-                self.__control_points.append(bezier)
+        else:
+            ## NEED to handle line between start/end
+            bezier_segments = []
+
+        self.__control_points = ControlPointList(bezier_segments)
+        if start_region is not None:
+            self.__start_point = np.array(start_region.centroid.coords[0])
+        elif connecting_path is not None:
+            self.__start_point = point_from_bezier(0, bezier_segments[0])
+        else:
+            self.__start_point = None
+        self.__end_region = end_region
+        if end_region is not None:
+            self.__end_point = np.array(end_region.centroid.coords[0])
+        elif connecting_path is not None:
+            self.__end_point = point_from_bezier(3, bezier_segments[-1])
+        else:
+            self.__end_point = None
 
     @property
     def start_point(self):
@@ -133,12 +238,6 @@ class PathSegment(object):
     @property
     def control_points(self):
         return self.__control_points
-
-    def join(self, next_segment):
-        # Adjust position and slope where next_segment joins
-        last_control = self.__control_points[-1]
-        last_control.set_position(next_segment.start_point)
-        last_control.smooth_slope(next_segment.control_points[0].derivative)
 
 #===============================================================================
 
@@ -200,19 +299,45 @@ class Sheath(object):
         node_geometry = self.__path_network.nodes(data='geometry')
         for path_id, path_nodes in self.__continuous_paths.items():
             # First derive the segments that connect the path's nodes
-            path_segments = []
+            centrelines = []
+            node_regions = []
             for node_1, node_2 in pairwise(path_nodes):
                 centreline = self.__get_centreline(node_1, node_2)
-                segment = PathSegment(node_geometry[node_1], centreline, node_geometry[node_2])
-                if len(path_segments) > 0:
-                    path_segments[-1].join(segment)
+                if len(centrelines) > 0:
+                    ##join_region = node_geometry[node_1]
+                    # Join previous and current centreline in circle centred at region's centroid
+                    join_region = node_geometry[node_1].centroid.buffer(2*SHEATH_WIDTH)
+                    joined_beziers = join_beziers_in_region(centrelines[-1], join_region, centreline)
+
+                    # Adjust previous centreline
+                    segments = centrelines[-1].asSegments()
+                    segments[-1] = joined_beziers[0]
+                    centrelines[-1] = BezierPath.fromSegments(segments)
+
+                    # Add centreline of join
+                    centrelines.append(BezierPath.fromSegments(joined_beziers[1:2]))
+                    node_regions.append((None, None))
+
+                    # Adjust current centreline
+                    segments = centreline.asSegments()
+                    segments[0] = joined_beziers[2]
+                    centreline = BezierPath.fromSegments(segments)
+
+                centrelines.append(centreline)
+                node_regions.append((node_geometry[node_1], node_geometry[node_2]))
+            path_segments = []
+            for n, centreline in enumerate(centrelines):
+                regions = node_regions[n]
+                segment = PathSegment(regions[0], centreline, regions[1])
                 path_segments.append(segment)
+
             # And use them to set the control points for the path's centreline region
             for segment in path_segments:
-                control_points = segment.control_points
-                if len(self.__control_points[path_id]) == 0:
-                    self.__control_points[path_id].append(control_points[0])
-                self.__control_points[path_id].extend(control_points[1:])
+                if segment is not None:
+                    control_points = segment.control_points
+                    if len(self.__control_points[path_id]) == 0:
+                        self.__control_points[path_id].append(control_points[0])
+                    self.__control_points[path_id].extend(control_points[1:])
             # The region starts and ends at the respective node centroids
             self.__control_points[path_id][0].set_position(path_segments[0].start_point)
             self.__control_points[path_id][-1].set_position(path_segments[-1].end_point)
