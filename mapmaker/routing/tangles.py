d4927ba18f7fac1908e7108a2cd8d9f8ced47ff5
David Brooks <dave@bcs.co.nz>
2021-10-03 11:12:33 +1300

    Generate permutations that permute all elements; tidy up code.

--- mapmaker/routing/tangles.py
+++ mapmaker/routing/tangles.py
@@ -1,31 +1,67 @@
-def tangles(permutation, debug=False):
-    max_m = len(permutation) - 1
-    p = permutation.copy()
-    for n, m in enumerate(permutation):
+#===============================================================================
+#
+#  Flatmap viewer and annotation tools
+#
+#  Copyright (c) 2020-21  David Brooks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#===============================================================================
+
+import itertools
+
+#===============================================================================
+
+from permutation import Permutation
+
+#===============================================================================
+
+def tangles(permutation):
+    p = [a-1 for a in permutation.to_image()]
+    for n, m in enumerate(p):
         if n > m:   #  assert p[m] == n, i.e. p[p[n]] == n
             p[n] = n
-    if debug: print(permutation, '-->', p)
+    while len(p) > 1 and p[0] == p[-1]:
+        p = p[1:-1]
+    max_m = len(p) - 1
     tangles = 0
-    i = 0 if p[0] < max_m else 1
+    i = 0
     while i < max_m:
         m = p[i]
         j = i + 1
         while j < p[i]:
             if p[j] > m:
                 tangles += 1
-            if debug: print('  ', i, m, '  ', j, p[j], '  ', tangles)
             j += 1
         i += 1
-    if debug: print('    -->', tangles)
     return tangles
 
+#===============================================================================
 
-def test(p):
-    return tangles([a-1 for a in p])
+def permutations(N):
+    for p in itertools.permutations(range(N)):
+        if len([n for n, a in enumerate(p) if n == a]) == 0:
+            perm = Permutation(*[a+1 for a in p])
+            if len(perm.to_cycles()) == N/2:
+                yield perm
 
-permutations = [
-    [6, 3, 2, 5, 4, 1],
+#===============================================================================
+
+test_permutations = [
+    [1, 2, 3, 4, 5, 6, 7],
+    [6, 3, 2, 5, 4, 1, 7],
     [6, 4, 5, 2, 3, 1],
+    [6, 4, 3, 2, 7, 1, 5],
     [5, 3, 2, 6, 1, 4],
     [5, 4, 6, 2, 1, 3],
     [4, 6, 5, 1, 3, 2],
@@ -36,5 +72,17 @@ permutations = [
     [3, 4, 1, 2],
 ]
 
-for p in permutations:
-    print(p, '-->', test(p))
+#===============================================================================
+
+def test(N):
+    for perm in permutations(N):
+        t = tangles(perm)
+        print(t, perm.to_image())
+
+if __name__ == '__main__':
+    N = 6
+    for n in range(1, N+1):
+        test(n)
+        print()
+
+#===============================================================================
