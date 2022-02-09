1acbcded45af8ab23164876c0035821f65d7c6fc
David Brooks <dave@bcs.co.nz>
2022-02-10 08:41:51 +1300

    First version of a tool to add name fields to an anatomical mapping file.

---
+++ tools/anatomical_names.py
@@ -0,0 +1,21 @@
+import json
+from mapmaker.knowledgebase import KnowledgeStore
+
+def main():
+    knowledge_store = KnowledgeStore('/Users/dave/Flatmaps/map-server/flatmaps')
+    with open('anatomical_map.json', 'r') as fp:
+        anatomical_map = json.loads(fp.read())
+    named_map = {}
+    for cls, term in anatomical_map.items():
+        label = knowledge_store.label(term)
+        if label == term:
+            label = "term is unknown in SciCrunch..."
+        named_map[cls] = {
+            'term': term,
+            'name': label
+        }
+    with open('anatomical_map_with_names.json', 'w') as fp:
+        fp.write(json.dumps(named_map, indent=4))
+
+if __name__ == '__main__':
+    main()
