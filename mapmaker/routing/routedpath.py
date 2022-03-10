1fc41ccda6e2468448502263042461bc389cdf46
David Brooks <dave@bcs.co.nz>
2022-03-10 14:08:37 +1300

    Tweak display of Bezier control points.

--- mapmaker/routing/routedpath.py
+++ mapmaker/routing/routedpath.py
@@ -91,10 +91,10 @@ def bezier_control_points(bezier, label=''):
     geometry = []
     bz_pts = tuple(point_to_coords(p) for p in bezier.points)
     for pt in (bz_pts[0], bz_pts[3]):
-        geometry.append(GeometricShape.circle(pt, radius=1000,
+        geometry.append(GeometricShape.circle(pt, radius=500,
             properties={'type': 'bezier', 'kind': 'bezier-end'}))
     for pt in bz_pts[1:3]:
-        geometry.append(GeometricShape.circle(pt, radius=1000,
+        geometry.append(GeometricShape.circle(pt, radius=400,
             properties={'type': 'bezier', 'kind': 'bezier-control', 'label': label}))
     geometry.append(GeometricShape.line(*bz_pts[0:2], properties={'type': 'bezier'}))
     geometry.append(GeometricShape.line(*bz_pts[2:4], properties={'type': 'bezier'}))
