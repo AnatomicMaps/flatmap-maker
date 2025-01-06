68da28999e882bcd3cc39f1cc22baf32a7c29f94
David Brooks <d.brooks@auckland.ac.nz>
2025-01-06 16:17:55 +1300

    Put code for text identification within a shape into its own file.

--- mapmaker/shapes/classify.py
+++ mapmaker/shapes/classify.py
@@ -32,6 +32,8 @@ import shapely.strtree
 from mapmaker.shapes import Shape, SHAPE_TYPE
 from mapmaker.utils import log
 
+from .text_finder import TextFinder
+
 #===============================================================================
 
 """
@@ -53,36 +55,6 @@ Shape types from size (area and aspect ratio) and geometry:
 
 """
 
-# SVG pixel space scaled to world metres
-# These are based on the CVS bondgraph diagram
-
-MAX_TEXT_VERTICAL_OFFSET = 5    # Between cluster baseline and baselines of text in the cluster
-TEXT_BASELINE_OFFSET = -14.5    # From vertical centre of a component
-
-#===============================================================================
-
-class TextShapeCluster:
-    def __init__(self, shape: Optional[Shape]=None):
-        self.__shapes: list[Shape] = []
-        self.__baselines: float = 0
-        if shape is not None:
-            self.add_shape(shape)
-
-    @property
-    def baseline(self) -> float:
-        return self.__baselines/len(self.__shapes) if len(self.__shapes) else 0
-
-    @property
-    def shapes(self) -> list[Shape]:
-        return self.__shapes
-
-    def add_shape(self, shape: Shape):
-        self.__shapes.append(shape)
-        self.__baselines += shape.baseline
-
-    def left_sort_shapes(self):
-        self.__shapes.sort(key=lambda s: s.left)
-
 #===============================================================================
 
 class ShapeClassifier:
@@ -90,8 +62,8 @@ class ShapeClassifier:
         self.__shapes = list(shapes)
         self.__shapes_by_type: DefaultDict[SHAPE_TYPE, list[Shape]] = defaultdict(list[Shape])
         self.__geometry_to_shape: dict[int, Shape] = {}
-        self.__max_text_vertical_offset = metres_per_pixel * MAX_TEXT_VERTICAL_OFFSET
-        self.__text_baseline_offset = metres_per_pixel * TEXT_BASELINE_OFFSET
+        self.__text_finder = TextFinder(metres_per_pixel)
+
         geometries = []
         for n, shape in enumerate(shapes):
             geometry = shape.geometry
@@ -155,46 +127,8 @@ class ShapeClassifier:
     #=================================
         for shape in self.__shapes:
             if shape.shape_type in [SHAPE_TYPE.COMPONENT, SHAPE_TYPE.CONTAINER]:
-                self.__set_label(shape)
+                if (label := self.__text_finder.get_text(shape)) is not None:
+                    shape.properties['label'] = label
         return [s for s in self.__shapes if not s.exclude]
-
-    def __set_label(self, shape: Shape):
-    #===================================
-        text_shapes = [s for s in shape.children if s.shape_type == SHAPE_TYPE.TEXT]
-        text_clusters = self.__cluster_text(text_shapes)
-        text_baseline = (shape.geometry.bounds[1] + shape.geometry.bounds[3])/2 + self.__text_baseline_offset
-        base_text = superscript = subscript = ''
-        for cluster in text_clusters:
-            if (cluster.baseline + self.__max_text_vertical_offset) < text_baseline:
-                subscript = self.__text_block_to_text(cluster.shapes)
-            elif (cluster.baseline - self.__max_text_vertical_offset) > text_baseline:
-                superscript = self.__text_block_to_text(cluster.shapes)
-            else:
-                base_text = self.__text_block_to_text(cluster.shapes)
-        label = f'${base_text}{f"_{{{subscript}}}" if subscript != "" else ""}{f"^{{{superscript}}}" if superscript != "" else ""}$'
-        if label != '':
-            shape.properties['label'] = label
-
-    def __text_block_to_text(self, text_block: list[Shape]) -> str:
-    #==============================================================
-        return f'{''.join([s.text for s in text_block])}'.replace(' ', '\\ ')
-
-    def __cluster_text(self, text_shapes: list[Shape]) -> list[TextShapeCluster]:
-    #============================================================================
-        baseline_ordered_shapes = sorted(text_shapes, key=lambda s: s.baseline)
-        clusters: list[TextShapeCluster] = []
-        current_cluster = None
-        for shape in baseline_ordered_shapes:
-            if (current_cluster is None
-             or (shape.baseline - current_cluster.baseline) > self.__max_text_vertical_offset):
-                current_cluster = TextShapeCluster(shape)
-                clusters.append(current_cluster)
-            else:
-                # Note: ``current_cluster.baseline`` is monotonically increasing
-                current_cluster.add_shape(shape)
-            shape.properties['exclude'] = True
-        for cluster in clusters:
-            cluster.left_sort_shapes()
-        return clusters
 
 #===============================================================================
---
+++ mapmaker/shapes/text_finder.py
@@ -0,0 +1,103 @@
+#===============================================================================
+#
+#  Flatmap viewer and annotation tools
+#
+#  Copyright (c) 2018 - 2024  David Brooks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#===============================================================================
+
+from typing import Optional
+
+#===============================================================================
+
+from . import Shape, SHAPE_TYPE
+
+#===============================================================================
+
+# SVG pixel space scaled to world metres -- these are based on the CVS bondgraph diagram
+
+MAX_TEXT_VERTICAL_OFFSET = 5    # Between cluster baseline and baselines of text in the cluster
+TEXT_BASELINE_OFFSET = -14.5    # From vertical centre of a component
+
+#===============================================================================
+
+class TextShapeCluster:
+    def __init__(self, shape: Optional[Shape]=None):
+        self.__shapes: list[Shape] = []
+        self.__baselines: float = 0
+        if shape is not None:
+            self.add_shape(shape)
+
+    @property
+    def baseline(self) -> float:
+        return self.__baselines/len(self.__shapes) if len(self.__shapes) else 0
+
+    @property
+    def shapes(self) -> list[Shape]:
+        return self.__shapes
+
+    def add_shape(self, shape: Shape):
+        self.__shapes.append(shape)
+        self.__baselines += shape.baseline
+
+    def left_sort_shapes(self):
+        self.__shapes.sort(key=lambda s: s.left)
+
+#===============================================================================
+
+class TextFinder:
+    def __init__(self, scaling: float):
+        self.__max_text_vertical_offset = scaling * MAX_TEXT_VERTICAL_OFFSET
+        self.__text_baseline_offset = scaling * TEXT_BASELINE_OFFSET
+
+    def get_text(self, shape: Shape) -> Optional[str]:
+    #=================================================
+        text_shapes = [s for s in shape.children if s.shape_type == SHAPE_TYPE.TEXT]
+        text_clusters = self.__cluster_text(text_shapes)
+        text_baseline = (shape.geometry.bounds[1] + shape.geometry.bounds[3])/2 + self.__text_baseline_offset
+        base_text = superscript = subscript = ''
+        for cluster in text_clusters:
+            if (cluster.baseline + self.__max_text_vertical_offset) < text_baseline:
+                subscript = self.__text_block_to_text(cluster.shapes)
+            elif (cluster.baseline - self.__max_text_vertical_offset) > text_baseline:
+                superscript = self.__text_block_to_text(cluster.shapes)
+            else:
+                base_text = self.__text_block_to_text(cluster.shapes)
+        text = f'${base_text}{f"_{{{subscript}}}" if subscript != "" else ""}{f"^{{{superscript}}}" if superscript != "" else ""}$'
+        return text if text != '' else None
+
+    def __text_block_to_text(self, text_block: list[Shape]) -> str:
+    #==============================================================
+        return f'{''.join([s.text for s in text_block])}'.replace(' ', '\\ ')
+
+    def __cluster_text(self, text_shapes: list[Shape]) -> list[TextShapeCluster]:
+    #============================================================================
+        baseline_ordered_shapes = sorted(text_shapes, key=lambda s: s.baseline)
+        clusters: list[TextShapeCluster] = []
+        current_cluster = None
+        for shape in baseline_ordered_shapes:
+            if (current_cluster is None
+             or (shape.baseline - current_cluster.baseline) > self.__max_text_vertical_offset):
+                current_cluster = TextShapeCluster(shape)
+                clusters.append(current_cluster)
+            else:
+                # Note: ``current_cluster.baseline`` is monotonically increasing
+                current_cluster.add_shape(shape)
+            shape.properties['exclude'] = True
+        for cluster in clusters:
+            cluster.left_sort_shapes()
+        return clusters
+
+#===============================================================================
