fc190974046eeb8b7738ec86109b0864d4032dc2
David Brooks <d.brooks@auckland.ac.nz>
2025-01-14 11:56:05 +1300

    SVG <image> elements only result in features in non-functional maps.

--- mapmaker/sources/svg/__init__.py
+++ mapmaker/sources/svg/__init__.py
@@ -391,16 +391,13 @@ class SVGLayer(MapLayer):
                     properties['stroke'] = element_style.get('stroke', 'none')
                 return Shape(shape_id, geometry, properties, svg_element=element)
         elif element.tag == SVG_TAG('image'):
-            if self.flatmap.map_kind != MAP_KIND.FUNCTIONAL:
-                geometry = None
-                clip_path_url = element_style.pop('clip-path', None)
-                if clip_path_url is not None:
-                    if ((geometry := self.__clip_geometries.get_by_url(clip_path_url)) is None
-                    and (clip_path_element := self.__definitions.get_by_url(clip_path_url)) is not None):
-                        T = transform@self.__get_transform(wrapped_element)
-                        geometry = self.__get_clip_geometry(clip_path_element, T)
-                else:
-                    geometry = self.__get_geometry(element, properties, transform)
+            geometry = None
+            clip_path_url = element_style.pop('clip-path', None)
+            if clip_path_url is not None:
+                if ((geometry := self.__clip_geometries.get_by_url(clip_path_url)) is None
+                and (clip_path_element := self.__definitions.get_by_url(clip_path_url)) is not None):
+                    T = transform@self.__get_transform(wrapped_element)
+                    geometry = self.__get_clip_geometry(clip_path_element, T)
                 if geometry is not None:
                     return Shape(shape_id, geometry, properties, shape_type=SHAPE_TYPE.IMAGE, svg_element=element)
         elif element.tag == SVG_TAG('g'):
