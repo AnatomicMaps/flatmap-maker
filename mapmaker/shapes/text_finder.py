a0be2c4e6038b5e6b8235e248ed155762805eca4
David Brooks <d.brooks@auckland.ac.nz>
2025-02-10 11:08:41 +1300

    Rework shape text finder to handle sub/superscripts within sub/superscripts.

--- mapmaker/shapes/text_finder.py
+++ mapmaker/shapes/text_finder.py
@@ -39,6 +39,10 @@ class TextShapeCluster:
         return self.__baselines/len(self.__shapes) if len(self.__shapes) else 0
 
     @property
+    def left(self) -> float:
+        return self.__shapes[0].left
+
+    @property
     def shapes(self) -> list[Shape]:
         return self.__shapes
 
@@ -51,47 +55,119 @@ class TextShapeCluster:
 
 #===============================================================================
 
+class LatexMaker:
+    def __init__(self):
+        self.__latex = []
+        self.__state = 0
+        self.__subscripted = False
+        self.__text = []
+
+    @property
+    def latex(self) -> str:
+    #======================
+        self.__make_latex()
+        return ''.join(self.__latex)
+
+    def add_text(self, text: str, state: int):
+    #=========================================
+        if state != self.__state:
+            self.__make_latex()
+            self.__state = state
+        if text != '':
+            self.__text.append(text)
+
+    def __make_latex(self):
+    #======================
+        if len(self.__text):
+            if self.__state < 0:
+                self.__latex.append(f'_{{{''.join(self.__text)}}}')
+                self.__subscripted = True
+            elif self.__state > 0:
+                superscript = f'^{{{''.join(self.__text)}}}'
+                if self.__subscripted:
+                    self.__latex.insert(-1, superscript)
+                else:
+                    self.__latex.append(superscript)
+            else:
+                self.__latex.append(''.join(self.__text))
+                self.__subscripted = False
+            self.__text = []
+
+#===============================================================================
+
 class TextFinder:
     def __init__(self, scaling: float):
         self.__max_text_vertical_offset = scaling * MAX_TEXT_VERTICAL_OFFSET
         self.__text_baseline_offset = scaling * TEXT_BASELINE_OFFSET
 
-    def get_text(self, shape: Shape) -> Optional[str]:
-    #=================================================
+    def get_text(self, shape: Shape) -> Optional[tuple[str, list[Shape]]]:
+    #=====================================================================
         text_shapes = [s for s in shape.children if s.shape_type == SHAPE_TYPE.TEXT]
         text_clusters = self.__cluster_text(text_shapes)
-        text_baseline = (shape.geometry.bounds[1] + shape.geometry.bounds[3])/2 + self.__text_baseline_offset
-        base_text = superscript = subscript = ''
+        offset = self.__max_text_vertical_offset
+        baseline = (shape.geometry.bounds[1] + shape.geometry.bounds[3])/2 + self.__text_baseline_offset
+        state = 0
+        clusters = []
+        latex = LatexMaker()
+        used_text_shapes = []
         for cluster in text_clusters:
-            if (cluster.baseline + self.__max_text_vertical_offset) < text_baseline:
-                subscript = self.__text_block_to_text(cluster.shapes)
-            elif (cluster.baseline - self.__max_text_vertical_offset) > text_baseline:
-                superscript = self.__text_block_to_text(cluster.shapes)
+            if cluster.baseline < (baseline - offset):
+                if state > 0 and len(clusters):
+                    latex.add_text(self.__text_clusters_to_text(clusters), state)
+                    clusters = []
+                clusters.append(cluster)
+                state = -1
+            elif cluster.baseline > (baseline + offset):
+                if state < 0 and len(clusters):
+                    latex.add_text(self.__text_clusters_to_text(clusters), state)
+                    clusters = []
+                clusters.append(cluster)
+                state = 1
             else:
-                base_text = self.__text_block_to_text(cluster.shapes)
-        text = f'${base_text}{f"^{{{superscript}}}" if superscript != "" else ""}{f"_{{{subscript}}}" if subscript != "" else ""}$'
-        return text if text != '' else None
+                if state != 0 and len(clusters):
+                    latex.add_text(self.__text_clusters_to_text(clusters), state)
+                    clusters = []
+                latex.add_text(self.__text_block_to_text(cluster.shapes), 0)
+                state = 0
+            used_text_shapes.extend(cluster.shapes)
+        if len(clusters):
+            latex.add_text(self.__text_clusters_to_text(clusters), state)
+        text = f'${latex.latex}$'
+        return (text, used_text_shapes) if text != '' else None
 
     def __text_block_to_text(self, text_block: list[Shape]) -> str:
     #==============================================================
         return f'{''.join([s.text for s in text_block])}'.replace(' ', '\\ ')
 
+    def __text_clusters_to_text(self, text_clusters: list[TextShapeCluster]) -> str:
+    #===============================================================================
+        baseline = text_clusters[0].baseline
+        offset = 0.9*self.__max_text_vertical_offset
+        latex = LatexMaker()
+        for cluster in text_clusters:
+            if cluster.baseline < (baseline - offset):
+                latex.add_text(self.__text_block_to_text(cluster.shapes), -1)
+            elif cluster.baseline > (baseline + offset):
+                latex.add_text(self.__text_block_to_text(cluster.shapes), 1)
+            else:
+                latex.add_text(self.__text_block_to_text(cluster.shapes), 0)
+        return latex.latex
+
     def __cluster_text(self, text_shapes: list[Shape]) -> list[TextShapeCluster]:
     #============================================================================
-        baseline_ordered_shapes = sorted(text_shapes, key=lambda s: s.baseline)
+        offset = self.__max_text_vertical_offset
+        left_ordered_shapes = sorted(text_shapes, key=lambda s: s.left)
         clusters: list[TextShapeCluster] = []
         current_cluster = None
-        for shape in baseline_ordered_shapes:
+        for shape in left_ordered_shapes:
             if (current_cluster is None
-             or (shape.baseline - current_cluster.baseline) > self.__max_text_vertical_offset):
+             or abs(shape.baseline - current_cluster.baseline) > offset):
                 current_cluster = TextShapeCluster(shape)
                 clusters.append(current_cluster)
             else:
                 # Note: ``current_cluster.baseline`` is monotonically increasing
                 current_cluster.add_shape(shape)
             shape.properties['exclude'] = True
-        for cluster in clusters:
-            cluster.left_sort_shapes()
         return clusters
 
 #===============================================================================
