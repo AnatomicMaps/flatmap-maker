a1e85ab11da7ab4989b985e25e76a4c69334b7a3
napakalas <yuda.munarko@gmail.com>
2025-03-06 18:32:43 +1300

    Add __extract_rendered_hierarchy to generate connectivity and hierarchy.

--- mapmaker/properties/pathways.py
+++ mapmaker/properties/pathways.py
@@ -643,6 +643,86 @@ class Pathways:
             for nerve_id in nerves:
                 self.__paths_by_nerve_id[nerve_id].append(path_id)
 
+    def __extract_rendered_hierarchy(self, route_graphs, connectivity_graphs):
+        rendered_connectivities = []
+        hierachy = {}
+        rendered_features = []
+        for path_id, cg in connectivity_graphs.items():
+            if self.__resolved_pathways is not None and path_id in self.__resolved_pathways.paths_dict:
+                #restructure connectivity graph so it aligns to self.__resolved_pathways
+                resolved_nodes = set(self.__resolved_pathways.paths_dict[path_id].get('nodes', []))
+                removed_nodes = [node for node, node_dict in cg.nodes(data=True)
+                                if node_dict.get('type') == 'feature' and
+                                not {f.geojson_id for f in node_dict.get('features', [])} & resolved_nodes]
+                for node in removed_nodes:
+                    neighbors = list(cg.neighbors(node))
+                    predecessors = [n for n in neighbors if n == cg.edges[(node, n)]['predecessor']]
+                    successors = [n for n in neighbors if n == cg.edges[(node, n)]['successor']]
+                    predecessors, successors = (predecessors or neighbors), (successors or neighbors)
+                    cg.add_edges_from([(e_0, e_1, {'predecessor': e_0, 'successor': e_1})
+                                    for e_0 in predecessors for e_1 in successors if e_0 != e_1])
+                cg.remove_nodes_from(removed_nodes)
+
+                route_graph = route_graphs[path_id]
+                path_rendered_features, path_rendered_segments, path_rendered_nerves = [], [], []
+                segment_to_node = {}
+                # get node_hierarchy and rendered_nodes
+                for node, node_dict in cg.nodes(data=True):
+                    # get node_hierarchy
+                    rendered_node = tuple(node_dict['node'])
+                    root = rendered_node[1][-1] if rendered_node[1] else rendered_node[0]
+                    hierachy.setdefault(root, {'id': root, 'children': [], 'paths': []})['paths'].extend([path_id] * (path_id not in hierachy[root]['paths']))
+                    while len(rendered_node[1]) > 0:
+                        hierachy[root]['children'].append(rendered_node) if rendered_node not in hierachy[root]['children'] else None
+                        rendered_node = (rendered_node[1][0], rendered_node[1][1:])
+                    # get rendered features and organise segments based on nodes in connectivity_node
+                    if node_dict.get('type') == 'feature':
+                        path_rendered_features += [(f.id, f.geojson_id, path_id, node, node_dict['node']) for f in node_dict.get('features', []) if f.id in route_graph.nodes]
+                    if node_dict.get('type') in ['segment', 'no-segment']:
+                        nerveIds = {f.geojson_id: nerve_id for nerve_id in node_dict.get('nerve-ids', []) if (f := self.__flatmap.get_feature(nerve_id)) is not None}
+                        segment_to_node[tuple(node_dict['subgraph'].nodes)] = (node, node_dict['node'], nerveIds)
+
+                # get rendered segments and nerves
+                for n_0, n_1, edge_dict in nx.Graph(route_graph).edges(data=True):
+                    if (centreline_id := edge_dict.get('centreline')) and (nr_nodes := [v for k, v in segment_to_node.items() if {n_0, n_1}.issubset(k)]):
+                        path_rendered_segments += [
+                            (centreline_id, f.geojson_id if (f := self.__flatmap.get_feature(centreline_id)) else None,
+                             path_id, nr[0], nr[1], list(nr[2].keys()))
+                            for nr in nr_nodes
+                        ]
+                        path_rendered_nerves += [(nerve_id, fid, path_id, nr[0], nr[1]) for nr in nr_nodes for fid, nerve_id in nr[2].items()]
+
+                # get rendered_connectivities
+                path_rendered_nodes = {}
+                for _, fid, _, _, r_node in path_rendered_features:
+                    path_rendered_nodes.setdefault(r_node, {'rendered-node': r_node, 'featureIds': []})['featureIds'].append(fid)
+                for _, fid, _, _, r_node, nerveIds in path_rendered_segments:
+                    path_rendered_nodes.setdefault(r_node, {'rendered-node': r_node, 'segmentIds': [], **({'nerveIds': nerveIds} if nerveIds or None else {})})['segmentIds'].append(fid)
+                rendered_connectivities += [{
+                    'id': path_id,
+                    'connectivity': [(cg.nodes[edge[0]]['node'], cg.nodes[edge[1]]['node']) for edge in cg.edges],
+                    'rendered-nodes': list(path_rendered_nodes.values())
+                }]
+
+                # add path_rendered_features, path_rendered_segments, path_rendered_nerves to rendered_features
+                rendered_features += path_rendered_features + path_rendered_segments + path_rendered_nerves
+
+        rendered_nodes = {}
+        for id, featureId, path_id, node, rendered_node, *nerveIds in rendered_features:
+            rendered_nodes[featureId] = {
+                'id': id,
+                'featureId': featureId,
+                'path-ids': list({*rendered_nodes.get(featureId, {}).get('path-ids', []), path_id}),
+                'nodes': list({*rendered_nodes.get(featureId, {}).get('nodes', []), node}),
+                'rendered-node': rendered_node,
+                **({'nerveIds': nerveIds[0]} if nerveIds not in ([], [[]]) else {})
+            }
+        self.__rendered_data = {
+            'rendered-connectivities': rendered_connectivities,
+            'rendered-hierarchy': list(hierachy.values()),
+            'rendered-nodes': rendered_nodes
+        }
+
     def __route_network_connectivity(self, network: Network):
     #========================================================
         if self.__resolved_pathways is None:
@@ -746,6 +826,8 @@ class Pathways:
                     'pathways',
                     shapely.geometry.Polygon(feature.geometry.coords).buffer(0), properties)
                 layer.features.append(nerve_polygon_feature)
+
+        self.__extract_rendered_hierarchy(route_graphs, connectivity_graphs)
 
     def generate_connectivity(self, networks: Iterable[Network]):
     #============================================================
