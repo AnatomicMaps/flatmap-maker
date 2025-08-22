7d391fc42cd6001664c35361b599c1f127fa882c
David Brooks <d.brooks@auckland.ac.nz>
2025-08-22 20:14:51 +1200

    SVG: using an element's `id` attribute is in fact only for functional maps (#157).

--- mapmaker/sources/svg/__init__.py
+++ mapmaker/sources/svg/__init__.py
@@ -405,7 +405,9 @@ class SVGLayer(MapLayer):
         properties = parent_properties.copy()
         for name in NON_INHERITED_PROPERTIES:
             properties.pop(name, None)
-        if 'id' in element.attrib and element.tag != SVG_TAG('g'):
+        if (self.source.flatmap.map_kind == MAP_KIND.FUNCTIONAL
+          and 'id' in element.attrib
+          and element.tag != SVG_TAG('g')):
             properties['id'] = element.attrib.get('id')
         properties.update(properties_from_markup)
         shape_id = properties.get('id')  ## versus element.attrib.get('id')
